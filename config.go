56bfbe26ca7ffe873ada04683f957482a31ea71f
Akiba <akegarasu.akiba@gmail.com>
2022-04-16 11:09:39 +0800

    :art: 提示文本

--- config.go
+++ config.go
@@ -73,7 +73,7 @@ func generateDefaultConfig() {
 	sb := strings.Builder{}
 	sb.WriteString(defaultConfig)
 	_ = os.WriteFile("config.yml", []byte(sb.String()), 0o644)
-	log.Info("默认配置文件已生成，请修改 config.yml 后重新启动!")
+	log.Info("默认配置文件已生成, 请重新启动")
 }
 
 func saveConfig() {
